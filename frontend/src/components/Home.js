2339d49e232cc0b76228436707bd1faff628d654
luojxxx <luo.j2010@gmail.com>
2017-07-21 22:08:20 -0700

    Fixing lack of scroll on homepage on mobile

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -38,31 +38,3 @@ class Home extends Component {
 }
 
 export default Home;
-
-
-// <div className='homeContentContainer'>
-
-//   <div className='leftHomeContent'>
-
-//   <p> Benefits: <br />
-//   - Catching up on journaling is easier than ever<br />
-//   - Instantly record flashbacks of forgotten memories<br />
-//   - Use the context and reminders to recover lost memories<br />
-//   - Simple management, organization, and backup of your photos and videos <br />
-//   - Beautiful timeline layout of all your memories: journal entries, photos, and videos <br />
-//   - GIF support! Plus GIF previews for videos <br />
-//   - Quick search and access to any point in your life <br />
-//   - Companion AI to help you find what to say<br />
-//   - Have a journal that you can pass down to your family and children <br />
-//   </p>
-//   </div>
-
-//   <div className='rightHomeContent'>
-
-    
-//   </div>
-  
-// </div>
-
-//      <h3><a href={this.props.route.auth.login()}>Login via Dropbox</a></h3> <br/>
-// <Button type="button" class="btn btn-primary" onClick={this.props.route.auth.login()} >Login via Dropbox</Button>
