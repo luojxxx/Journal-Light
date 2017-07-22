a191ee81ae8d1b9b055beac5172af34cb2e2526c
luojxxx <luo.j2010@gmail.com>
2017-07-21 21:23:37 -0700

    Updated Homepage for better look

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-import fullpage from '../icons/fullpage.gif';
+import fullpage from '../icons/partialpage.gif';
 
 class Home extends Component {
   render() {
     return (
       <div className='homeContainer'>
-
+        <h3>Journal Light</h3> <br/>
+        
       <div className='homeContent'>
-        <h1>Journal Light</h1> <br/>
-        <img className='homeImage' src={fullpage} /> <br/>
-        <h4>Making it easier to catch up on your journal and fill in the blanks whether it was last week or the beginning of your life</h4>
-      </div>
+        <hr id='homeRuler' />
+        <div className='loginTextArea'>
+          <h2 className='noTopMargin'>Making it easier to view and archive your memories</h2>
+        </div>
 
-      <div className='loginArea'>
-        <h3><a className='loginButton' onClick={this.props.route.auth.login}>Login via Dropbox</a></h3> 
-        <h3><a className='loginButton' onClick={this.props.route.auth.guestLogin}>Login via Guest</a></h3> 
+        <div className='loginArea'>
+        <div id='featureList'>
+          <ul>
+            <li style={{fontWeight: 'bold', color:'tomato'}}>Giant Timeline View of your Life</li>
+            <li style={{fontWeight: 'bold', color:'#FFB347'}}>Photos, Gifs, Videos, and Video previews</li>
+            <li style={{fontWeight: 'bold', color:'#FFCB06'}}>Add new entries instantly anywhere</li>
+            <li style={{fontWeight: 'bold', color:'#52B280'}}>Entries don't require full date</li>
+            <li style={{fontWeight: 'bold', color:'#668DAA'}}>Dropbox backup</li>
+            <li style={{fontWeight: 'bold', color:'#CFA3D8'}}>Companion AI</li>
+          </ul>
+        </div>
+          <div id='buttonArea'>
+            <h3 className='loginButtonWrap'><a className='loginButton' onClick={this.props.route.auth.login}>Login via Dropbox</a></h3> 
+            <h3 className='loginButtonWrap'><a className='loginButton' onClick={this.props.route.auth.guestLogin}>Login via Guest</a></h3>
+          </div>
+        </div>
+
+        <img className='homeImage' src={fullpage} /> <br/>
       </div>
       </div>
     );
